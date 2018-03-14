78c21771c0d7f803078a1540ec12af30cc798d0d
PJ Frias <peejfrias@gmail.com>
2018-03-14 13:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ class Driver {
 
   constructor(name, date) {
     this.name = name;
-    this.date = date;
+    this.startDate = new Date(date);
   }
-
-  startDate() {
-     return new Date(this.date);
-   }
 
   yearsExperienceFromBeginningOf(){
 
