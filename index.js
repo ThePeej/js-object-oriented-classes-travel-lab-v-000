2e5d6285a121c740e5f2ca4162b98ed96b856b93
PJ Frias <peejfrias@gmail.com>
2018-03-14 13:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ class Driver {
 class Route {
 
   constructor(beginningLocation, endingLocation) {
-
+    this.start = beginningLocation;
+    this.end = endingLocation;
   }
 
 // let route = new Route({horizontal: 'Park', vertical: '34'}, {horizontal: 'Park', vertical: '45'})
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     let horizontal = parseInt(endingLocation.vertical) - parseInt(beginningLocation.vertical);
-    let vertical = 0
+    let vertical = 0;
     return horizontal + vertical
   }
 
