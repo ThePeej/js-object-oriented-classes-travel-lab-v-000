43531bb686824db11e6b1746d02ada3fb6e7e97a
PJ Frias <peejfrias@gmail.com>
2018-03-14 13:37:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Route {
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     let horizontal = parseInt(endingLocation.vertical) - parseInt(beginningLocation.vertical);
-    let vertical;
+    let vertical = 0
     return horizontal + vertical
   }
 
