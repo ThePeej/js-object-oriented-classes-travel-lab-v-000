b6930c1bd681dd48568880c33e740598a04c01e7
PJ Frias <peejfrias@gmail.com>
2018-03-14 13:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ class Route {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
     let vertical = parseInt(this.end.vertical) - parseInt(this.start.vertical);
     let horizontal = eastWest.indexOf(this.end.horizontal) - eastWest.indexOf(this.start.horizontal);
-    debugger;
     return horizontal + vertical
   }
 
